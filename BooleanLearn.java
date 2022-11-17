c3e7ac0e1dad4d975b1e1ecc0eec1d9c852b25dc
wh.q <dawangong@126.com>
2022-11-17 14:56:25 +0800

    feat: boolean

---
+++ BooleanLearn.java
@@ -0,0 +1,10 @@
+public class BooleanLearn {
+  public static void main(String[] args) {
+    boolean pass = true; // 只有true 和 false 不能 0 之类的
+    if(pass == true) {
+      System.out.println("pass");
+    } else {
+      System.out.println("no pass");
+    }
+  }
+}
